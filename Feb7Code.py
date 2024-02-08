c37113ea1ed597b617b693d137925f36a19900eb
Jack Jin <79951112+Rvistix@users.noreply.github.com>
2024-02-08 09:28:06 -0500

    forgot to push last night.

--- Feb7Code.py
+++ Feb7Code.py
@@ -40,8 +40,11 @@ table = tables.getSubTable('vision')
 camera = CameraServer.startAutomaticCapture()
 camera.setResolution(640, 480)
 cvsinkVIDEO = CameraServer.getVideo()
+cvSource = cvsinkVIDEO.getSource()
+cvSource.setFPS(30)
 emptyNArray = np.zeros((640, 480, 3))
 videoOutput = CameraServer.putVideo("I HATE PYTHON", 640, 480)
+binOutput = CameraServer.putVideo("Tuning", 640, 480)
 detector = Detector(families='tag36h11',
                        nthreads=1,
                        quad_decimate=1.0,
@@ -52,9 +55,6 @@ detector = Detector(families='tag36h11',
 
 def findAB(target):
     a = 0.0
-
-   
-
     for i in XYZ: 
         if target == 0: 
             continue
@@ -63,22 +63,19 @@ def findAB(target):
             break
         else:
             a = b
-
-
     return a, b
 
-def correct(target): 
+def correct(gay, target): 
+
+    if gay or target > 5.1527 or target < -5.1527:
+        return target * 39.3701
+
     bo = False
-    if target < 0: 
-        target = target * -1
+    if target < 0:
+        target = -1 * target
         bo = True
-        
 
     a, b = findAB(target)
-    if a == -1:
-        return 69
-
-    print("A: %f, B: %f" % (a, b))
 
     x = b - a
     diff1 = target - a
@@ -86,7 +83,15 @@ def correct(target):
 
     diff2 = XYZ[b] - XYZ[a]
     add = ratio * diff2
-    return bo ? (-1 * (XYZ[a] + add) : (XYZ[a] + add)
+
+#    print(XYZ[a] + add, flush=True)
+
+    if bo:
+        return (-1 * (XYZ[a] + add))
+    else:
+        return XYZ[a] + add
+
+
 
 
 
@@ -98,16 +103,16 @@ def resetApriltagstuff():
         table.putString("Apriltag ID:{a}".format(a=str(i)), "NOT DETECTED!")
 
 def updateApriltagStuff(id, poseT):
+    x = pose[2]
     y = -pose[0]
     z = -pose[1]
-    x = pose[2]
     table.putBoolean("Apriltag {a} Detected".format(a=id), True)
     table.putNumber("Apriltag {a} X: ".format(a=id), x)
     table.putNumber("Apriltag {a} Y: ".format(a=id), y)
-    table.putNumber("Apriltag {a} Z: ".format(a=id), math.sqrt(pow(x, 2) +  pow(y, 2)))
-    table.putNumber("ApriltagCorrected {a} X: ".format(a=id), correct(x))
-    table.putNumber("ApriltagCorrected {a} Y: ".format(a=id), correct(y))
-    table.putNumber("ApriltagCorrected {a} Z: ".format(a=id), math.sqrt(pow(correct(x), 2) +  pow(correct(y), 2)))
+    table.putNumber("Apriltag {a} Z: ".format(a=id), z)
+    table.putNumber("ApriltagCorrected {a} X: ".format(a=id), correct(False, x))
+    table.putNumber("ApriltagCorrected {a} Y: ".format(a=id), correct(True, y))
+    table.putNumber("ApriltagCorrected {a} Z: ".format(a=id), correct(True, z))
 
 def updateApriltagStuffgay(id):
     table.putBoolean("Apriltag {a}".format(a=id), False)
@@ -117,6 +122,21 @@ def updateApriltagStuffgay(id):
     table.putNumber("ApriltagCorrected {a} X: ".format(a=id), 0.0)
     table.putNumber("ApriltagCorrected {a} Y: ".format(a=id), 0.0)
     table.putNumber("ApriltagCorrected {a} Z: ".format(a=id), 0.0)
+
+def drawBoxesAndStuff(r,processedImage):
+    updateApriltagStuff(r.tag_id, pose)
+    (ptA, ptB, ptC, ptD) = r.corners
+    ptB = (int(ptB[0]), int(ptB[1]))
+    ptC = (int(ptC[0]), int(ptC[1]))
+    ptD = (int(ptD[0]), int(ptD[1]))
+    ptA = (int(ptA[0]), int(ptA[1]))
+    cv2.line(processedImage, ptA, ptB, (0, 255, 0), 2)
+    cv2.line(processedImage, ptB, ptC, (0, 255, 0), 2)
+    cv2.line(processedImage, ptC, ptD, (0, 255, 0), 2)
+    cv2.line(processedImage, ptD, ptA, (0, 255, 0), 2)
+    (cX, cY) = (int(r.center[0]), int(r.center[1]))
+    cv2.putText(processedImage, str(r.tag_id), (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_4, False)
+    return processedImage
 
 while True:
     curCorrectionFactor = getCorrectionFactors()
@@ -128,8 +148,9 @@ while True:
     image32 = np.float32(image)
     gray = cv2.cvtColor(image32, cv2.COLOR_BGR2GRAY)
     gray = gray.astype(np.uint8)
+    temp, bin = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
     processedImage = image32
-    results = detector.detect(gray, True, params, 0.1524)
+    results = detector.detect(bin, True, params, 0.1524)
     numOfATags = len(results)
     new_frame_time = time.time()
     fps = 1/ (new_frame_time - prev_frame_time)
@@ -147,17 +168,7 @@ while True:
             table.putString("Objective AprilTag Detected?", "Not Detected!")
             continue
         pose = r.pose_t
-        updateApriltagStuff(r.tag_id, pose)
-        (ptA, ptB, ptC, ptD) = r.corners
-        ptB = (int(ptB[0]), int(ptB[1]))
-        ptC = (int(ptC[0]), int(ptC[1]))
-        ptD = (int(ptD[0]), int(ptD[1]))
-        ptA = (int(ptA[0]), int(ptA[1]))
-        cv2.line(processedImage, ptA, ptB, (0, 255, 0), 2)
-        cv2.line(processedImage, ptB, ptC, (0, 255, 0), 2)
-        cv2.line(processedImage, ptC, ptD, (0, 255, 0), 2)
-        cv2.line(processedImage, ptD, ptA, (0, 255, 0), 2)
-        (cX, cY) = (int(r.center[0]), int(r.center[1]))
-        cv2.putText(processedImage, str(r.tag_id), (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_4, False)
-        
+        drawBoxesAndStuff(r, processedImage)
+        drawBoxesAndStuff(r, bin)
+    binOutput.putFrame(bin)
     videoOutput.putFrame(processedImage)
